f06987f0ac7b875c20e1ed9fc8aa531abf15582a
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2021-07-21 21:43:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,15 @@
 import os
 import requests
 import string
-stream = os.popen("curl --silent http://127.0.0.1:4040/api/tunnels | jq .tunnels[0].public_url")
+stream = os.popen(curl --silent http://127.0.0.1:4040/api/tunnels | jq .tunnels[0].public_url)
 output = stream.read()
 
 sample_str = output
 # Get last 3 character
 last_chars = sample_str[-21:]
 
+url1= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text=MAC-VNC'
 
-url= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text=MAC-VNC={}'.format(last_chars)
-requests.get(url)
+url2= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text={}'.format(last_chars)
+requests.get(url1)
+requests.get(url2)
