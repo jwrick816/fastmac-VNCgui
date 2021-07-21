44c5d0a9711700ab15662783905068f8d0d1f13f
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2021-07-21 21:36:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import string
-stream = os.popen("curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'")
+stream = os.popen("curl --silent http://127.0.0.1:4040/api/tunnels | jq .tunnels[0].public_url")
 output = stream.read()
 
 sample_str = output
