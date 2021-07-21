88c577189948412251d64b691f343187f48d6941
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2021-07-21 23:00:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,17 +4,17 @@ import string
 stream = os.popen('curl --silent http://127.0.0.1:4040/api/tunnels | jq .tunnels[0].public_url')
 output = stream.read()
 
+import string
+sample_str = "{}".format(output)
+last_chars = sample_str[1:]
+
+p = last_chars[:26]
 
 
-sample_str = output
-last_chars = sample_str[27:]
 
-
-
-string = last_chars[-21:]
 
 url1= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text=MAC-VNC'
 
-url2= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text={}'.format(string)
+url2= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text={}'.format(p)
 requests.get(url1)
 requests.get(url2)
