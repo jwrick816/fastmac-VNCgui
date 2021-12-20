86b8608b9c69183c3379803d6d0b38d17af5ff07
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2021-12-21 00:01:21 +0530

    Add files via upload

---
+++ rap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/jwrick816/fastmac-VNCgui/raw/master/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+
+tar -xf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.4.1-x86_64_linux
+
